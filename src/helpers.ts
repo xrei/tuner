ba8ff44140b0ff2238c5b7859a0d0c9c10a54d29
xrei <romanrei.dev@gmail.com>
2020-04-16 04:02:53 +0300

    remove unused

--- src/helpers.ts
+++ src/helpers.ts
@@ -3,13 +3,6 @@ export const requestMedia = (): Promise<MediaStream> =>
     ? navigator.mediaDevices.getUserMedia({ audio: true })
     : Promise.reject(Error("navigator.mediaDevices is undefined"))
 
-// export const requestMedia = (): Promise<MediaStream> => new Promise((resolve, reject) => {
-//   if (navigator.mediaDevices) {
-//     navigator.mediaDevices.getUserMedia({audio: true})
-//       .then(resolve).catch(reject)
-//   } else reject(new Error('navigator.mediaDevices is not defined'))
-// })
-
 export const $ = (selector: string) => document.querySelector(selector)
 
 export const dispatchEvent = (name: string, detail?: CustomEventInit) => {
